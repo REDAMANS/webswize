48299c6e4c2fa57a45d0af06344144d2375089f7
Webswize <webswize@gmail.com>
2023-09-05 00:18:23 +0100

    added custom 404

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -16,14 +16,8 @@ const Header = ({ navbar }: { navbar: Navbar }) => {
         <header className="fixed w-full top-0 backdrop-blur z-[100]">
             <nav className="nav__padding border-b z-50 flex text-sm text-gray-700 items-center justify-between flex-row">
                 <div className="flex flex-row md:gap-10 lg:gap-16 items-center flex-shrink">
-                    <Image className="h-auto" src="/assets/logo/logo-pic.svg" alt="logo" width={80} height={80} />
+                    <Image priority className="h-auto" src="/assets/logo/logo-pic.svg" alt="logo" width={80} height={80} />
                     <Links links={navbar.links} />
-                </div>
-                <div className="hidden md:flex items-center gap-8">
-                    <button aria-label="connecter" className="login overflow-hidden hover:text-white relative px-4 py-2.5 border border-blue-600 rounded-xl outline-none text-black">
-                        <Link href={navbar.authLinks[0].href}>{navbar.authLinks[0].name}</Link>
-                    </button>
-                    <Link className="link" href={navbar.authLinks[1].href}>{navbar.authLinks[1].name}</Link>
                 </div>
                 <SidebarProvider>
                     <Sidebar links={navbar.links} />
--- src/components/header/SidebarButton.tsx
+++ src/components/header/SidebarButton.tsx
@@ -1,16 +1,10 @@
 "use client"
 import { SidebarContext } from "@/context/SidebarContext";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 
 const SidebarButton = () => {
 
-    const { updateSidebar, isSidebarOpen } = useContext(SidebarContext);
-
-    useEffect(() => {
-      console.log(window)
-
-    }, [])
-    
+    const { updateSidebar, isSidebarOpen } = useContext(SidebarContext);    
 
     return (
         <button onClick={updateSidebar} className={`${isSidebarOpen && "sidebar__toggled"} z-10 sidebar__toggle h-10 w-10 flex md:hidden flex-col gap-1 items-center justify-center rounded-full`}>
