18959b6a71cc9f486e81df89e6fd43c372d9368b
Webswize <webswize@gmail.com>
2023-09-15 23:51:33 +0100

    improved Web vitals

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = ({ footer, links }: { footer: {links: string[]}, links: {name: st
             <section className="flex py-10 gap-20 md:gap-0 flex-col md:flex-row justify-between items-center">
                 <div className="flex flex-col md:items-start items-center flex-[1.5] justify-between gap-10 md:gap-0 md:flex-row w-full max-w-[551px]">
                     <div className="flex flex-col items-center md:items-start gap-8">
-                        <h6 className="font-semibold">{footer.links[0]}</h6>
+                        <p className="font-semibold">{footer.links[0]}</p>
                         <ul className="flex flex-col gap-4 text-sm text-gray-700">
                             {
                                 links.map((link: {name: string, href: string}, index: number) => (
