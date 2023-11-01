7b17a554dc2df9d6dfa51725360234dcf2bd7f84
Webswize <webswize@gmail.com>
2023-11-01 19:51:10 +0100

    added oauth redirection to previous page

--- src/components/auth/OAuthButton.tsx
+++ src/components/auth/OAuthButton.tsx
@@ -1,8 +1,11 @@
 "use client"
 import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
+import { useSearchParams } from "next/navigation";
 
 const OAuthButton = ({ provider, method }: { provider: any, method: string }) => {
+
+    const callbackUrl = useSearchParams().get("callbackUrl") as string;
 
     const providerImages = [
         {
@@ -11,7 +14,7 @@ const OAuthButton = ({ provider, method }: { provider: any, method: string }) =>
         }
     ]
     return (
-        <button onClick={() => signIn(provider.id)} className="px-4 py-3 w-full text-sm sm:text-base flex-wrap shadow-md border flex items-center gap-2 sm:gap-4 justify-center rounded-xl outline-none">
+        <button onClick={() => signIn(provider.id, { callbackUrl })} className="px-4 py-3 w-full text-sm sm:text-base flex-wrap shadow-md border flex items-center gap-2 sm:gap-4 justify-center rounded-xl outline-none">
             <span>{method} {provider.name}</span>
             {providerImages.find(image => image.name === provider.name)?.image}
         </button>
